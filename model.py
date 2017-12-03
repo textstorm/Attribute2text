5cf312b98fbf4e33ea600741cd416f9a65c603f5
textstorm <jiangzhewise@gmail.com>
2017-12-03 14:59:20 +0800

    model 12_3 update

--- model.py
+++ model.py
@@ -24,13 +24,15 @@ class ReviewGenerator(object):
     self.dropout = args.dropout
     self.beam_width = args.beam_width
     self.max_grad_norm = args.max_grad_norm
-    self.rnn_initializer = tf.orthogonal_initializer()
+    #self.rnn_initializer = tf.orthogonal_initializer()
+    self.rnn_initializer = tf.random_uniform_initializer(-0.08, 0.08)
     self.sess = sess
 
     self.batch_size = args.batch_size
     self.learning_rate = args.learning_rate
 
-    self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+    # self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+    self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate)
     self.global_step = tf.Variable(0, trainable=False)
 
     self.add_placeholder()
@@ -40,7 +42,7 @@ class ReviewGenerator(object):
 
     init_op = tf.global_variables_initializer()
     self.sess.run(init_op)
-    
+
   def add_placeholder(self):
     """ add placeholder for data """
     self.user = tf.placeholder(tf.int32, [None], name="user")
@@ -54,7 +56,6 @@ class ReviewGenerator(object):
     """ model graph """
     with tf.variable_scope("review_generator"):
       attribute_output = self.attribute_encoder()
-      print attribute_output.get_shape().as_list()
       self.logits, self.infer_logits, self.infer_sample_id = self.sequence_decoder(
           attribute_output)
 
@@ -62,14 +63,18 @@ class ReviewGenerator(object):
     """ attribute encoder layer """
     with tf.variable_scope("attribute_encoder") as scope:
       user = tf.one_hot(self.user, self.user_size, dtype=tf.float32)
+      print user.get_shape().as_list()
       user_encode = self.attribute_layer(user, 'user_encode')
       product = tf.one_hot(self.product, self.book_size, dtype=tf.float32)
+      print product.get_shape().as_list()
       product_encode = self.attribute_layer(product, 'product_encode')
       rating = tf.one_hot(self.rating, self.rating_size, dtype=tf.float32)
+      print rating.get_shape().as_list()
       rating_encode = self.attribute_layer(rating, 'rating_encode')
 
       attr_concat = tf.concat([user_encode, product_encode, rating_encode], 1)
-      attribute_output = tf.layers.dense(attr_concat, self.hidden_size * self.num_layers, 
+      print attr_concat.get_shape().as_list()
+      attribute_output = tf.layers.dense(attr_concat, self.hidden_size * 2 * self.num_layers, 
           activation=tf.nn.tanh, use_bias=True, name="attribute_output")
     return attribute_output
 
@@ -87,6 +92,7 @@ class ReviewGenerator(object):
       embedding = self._build_embedding(self.vocab_size, self.embed_size, "embedding")
       embed_input = tf.nn.embedding_lookup(embedding, self.review_in)
 
+      attribute_output = tf.reshape(attribute_output, [-1, self.num_layers, 2, self.hidden_size])
       cell, decoder_initial_state = self._build_rnn_cell(self.hidden_size, self.forget_bias, 
           self.num_layers, attribute_output, self.rnn_initializer, self.dropout)
 
@@ -147,8 +153,17 @@ class ReviewGenerator(object):
         labels=self.review_out, logits=self.logits)
     weight = tf.sequence_mask(self.review_len, max_len, dtype=self.logits.dtype)
     self.loss_op = tf.reduce_sum(loss * weight) / tf.to_float(self.batch_size)
+    print self.loss_op.get_shape().as_list()
 
   def build_train(self):
+    params = tf.trainable_variables()
+    gradients = tf.gradients(self.loss_op, params)
+    clipped_gradients, gradient_norm = tf.clip_by_global_norm(
+        gradients, self.max_grad_norm)
+    self.train_op = self.optimizer.apply_gradients(
+        zip(clipped_gradients, params), global_step=self.global_step)
+
+  def build_train_(self):
     grads_and_vars = self.optimizer.compute_gradients(self.loss_op)
     grads_and_vars = [(tf.clip_by_norm(g, self.max_grad_norm), v) for g, v in grads_and_vars]
     self.train_op = self.optimizer.apply_gradients(grads_and_vars, 
@@ -174,6 +189,7 @@ class ReviewGenerator(object):
         initializer=initializer,
         state_is_tuple=True)
     if dropout > 0.0:
+      print "use dropout"
       cell = tf.contrib.rnn.DropoutWrapper(cell=cell, input_keep_prob=(1.0 - dropout))
     return cell
 
@@ -188,19 +204,24 @@ class ReviewGenerator(object):
     if num_layers == 1:
       cell = cell_list[0]
     else:
-      cell = tf.contrib.rnn.MultiRNNCell(cell_list)
+      cell = tf.contrib.rnn.MultiRNNCell(cell_list, state_is_tuple=True)
 
-    cell_state = attribute_output
-    hidden_state = attribute_output
-    initial_state = tf.contrib.rnn.LSTMStateTuple(c=cell_state, h=hidden_state)
-
-    print initial_state.c
-    print initial_state.h
+    print attribute_output.get_shape().as_list()
+    attribute_output = tf.transpose(attribute_output, [1, 2, 0, 3])
+    print attribute_output.get_shape().as_list()
+    layer_unpacked = tf.unstack(attribute_output, axis=0)
+    lstm_state_tuple = tuple(
+      [tf.contrib.rnn.LSTMStateTuple(layer_unpacked[layer][0], layer_unpacked[layer][1]) 
+      for layer in range(self.num_layers)])
+    # cell_state, hidden_state = tf.split(attribute_output, 2, 0)
+    # print cell_state.get_shape().as_list()
+    # print hidden_state.get_shape().as_list()
+    #initial_state = tf.contrib.rnn.LSTMStateTuple(c=cell_state, h=hidden_state)
 
     if self.beam_width > 0:
-      decoder_initial_state = tf.contrib.seq2seq.tile_batch(initial_state, self.beam_width)
+      decoder_initial_state = tf.contrib.seq2seq.tile_batch(lstm_state_tuple, self.beam_width)
     else:
-      decoder_initial_state = initial_state
+      decoder_initial_state = lstm_state_tuple
     return cell, decoder_initial_state
 
   def _get_infer_maximum_iterations(self):
@@ -221,6 +242,9 @@ class ReviewGenerator(object):
     return tf.get_variable(shape=shape, initializer=initializer, name=name)
 
   def _build_embedding(self, vocab_size, embed_size, name):
+    initializer = tf.random_uniform_initializer(-0.08, 0.08)
     with tf.variable_scope("embedding") as scope:
-      embedding = self._weight_variable([vocab_size, embed_size], name=name)
+      embedding = self._weight_variable([vocab_size, embed_size], 
+                                        name=name, 
+                                        initializer=initializer)
     return embedding
