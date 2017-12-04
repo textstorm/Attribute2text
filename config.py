a9a8e36c47fafd8e81c1ae7e389d46d5ac84e906
textstorm <jiangzhewise@gmail.com>
2017-12-04 15:43:22 +0800

    config modified

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ def get_args():
   parser.add_argument('--dev_dir', type=str, default='../data/dev.txt', help='dev data dir')
   parser.add_argument('--test_dir', type=str, default='../data/test.txt', help='test data dir')
   parser.add_argument('--vocab_dir', type=str, default='../data/vocab.txt', help='vocab dir')
-  parser.add_argument('--save_dir', type=str, default='save', help='save directory')
-  parser.add_argument('--batch_size', type=int, default=256, help='train batch size')
+  parser.add_argument('--save_dir', type=str, default='../save', help='save directory')
+  parser.add_argument('--batch_size', type=int, default=64, help='train batch size')
   parser.add_argument('--eos', type=str, default='</s>', help='eos')
 
   parser.add_argument('--attribute_size', type=int, default=64, help='dimension of attribute')
@@ -28,9 +28,9 @@ def get_args():
   parser.add_argument('--attention_option', type=str, default='bahdanau', help='attention option')
   parser.add_argument('--beam_width', type=int, default=0, help='width of beam search')
 
-  parser.add_argument('--nb_epoch', type=int, default=60, help='number of epoch')
+  parser.add_argument('--nb_epoch', type=int, default=100, help='number of epoch')
   parser.add_argument('--max_step', type=int, default=10000, help='max train step')
-  parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')
+  parser.add_argument('--learning_rate', type=float, default=0.002, help='learning rate')
   parser.add_argument('--dropout', type=float, default=0., help='dropout rate')
   parser.add_argument('--max_grad_norm', type=float, default=5.0, help='max norm of gradient')
   parser.add_argument('--print_period', type=int, default=1, help='print information period')
